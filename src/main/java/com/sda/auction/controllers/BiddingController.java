96f53363ab65768f00efb04eb522dc119cfd4a83
Dan Osolos <dan.stefan75@yahoo.com>
2020-12-20 09:19:30 +0200

    Added @AllArgsConstructor(onConstructor = @__({@Autowired}))

--- src/main/java/com/sda/auction/controllers/BiddingController.java
+++ src/main/java/com/sda/auction/controllers/BiddingController.java
@@ -1,15 +1,14 @@
 package com.sda.auction.controllers;
 
 import com.sda.auction.services.BiddingService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor(onConstructor = @__({@Autowired}))
 public class BiddingController {
 
     private final BiddingService biddingService;
-
-    public BiddingController(BiddingService biddingService) {
-        this.biddingService = biddingService;
-    }
 
 }
